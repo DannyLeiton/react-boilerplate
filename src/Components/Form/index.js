6d9198fc21997c44baaa74ad4c7a7ff693da8054
Daniel Leiton <dleitonrivera@gmail.com>
2018-07-07 18:41:34 -0600

    Added axios error handling

--- src/Components/Form/index.js
+++ src/Components/Form/index.js
@@ -4,28 +4,39 @@ import axios from 'axios';
 import './styles.css';
 
 class Form extends Component {
-    state = { userName: '' }
+    state = { userName: '', error: null, disableButton: false }
   
     handleSubmit = (event) => {
-      const { userName } = this.state
       event.preventDefault();
+      this.setState({ ...this.state, disableButton: true });
+      const { userName } = this.state
       axios.get(`https://api.github.com/users/${userName}`)
         .then(resp => {
           this.props.onSubmit(resp.data);
-          this.setState({ userName: '' });
+          this.setState({ userName: '', error: null, disableButton: false });
+        })
+        .catch(err => {
+            this.setState({userName, error: err.message, disableButton: false});
         });
     };
   
     render() {
+      const { userName, error, disableButton } = this.state;
+    
       return (
-        <form onSubmit={this.handleSubmit}>
-          <input className="Text-input"
-            type="text"
-            value={this.state.userName}
-            onChange={(event) => this.setState({ userName: event.target.value })}
-            placeholder="Type a Github username" required />
-          <button className="Submit-button" type="submit">Add Card</button>
-        </form>
+        <div>
+            <form onSubmit={this.handleSubmit}>
+                <input className="Text-input"
+                    type="text"
+                    value={userName}
+                    onChange={(event) => this.setState({ ...this.state, userName: event.target.value })}
+                    placeholder="Type a Github username" required />
+                <button className="Submit-button" type="submit" disabled={disableButton}>Add Card</button>
+            </form>
+            <div className="Error-message">
+                {error}
+            </div>
+        </div>
       );
     }
   }
